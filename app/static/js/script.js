59a43b5e7f519e08ec34cf28450f2ffcfb2c56bd
MuhammadTahaMustafa-06 <k228735@nu.edu.pk>
2023-11-23 22:45:48 +0500

    SECOND COMMIT

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,105 +1,108 @@
-// Function to handle image upload
+   // Function to handle image upload
+   let imageData = null;
+let reader = null;
+
 function handleImageUpload(e) {
     const file = e.target.files[0];
     if (!file) return;
 
-    const reader = new FileReader();
+    reader = new FileReader();
     reader.onload = function () {
         document.getElementById('preview-img').src = reader.result;
-        sendToBackend(reader.result);
+        imageData = reader.result;
     };
     reader.readAsDataURL(file);
 }
 
-// Function to handle capturing an image
-function captureImage() {
-    const video = document.createElement("video");
-    const canvas = document.createElement("canvas");
-    const context = canvas.getContext('2d');
-    const imagePreview = document.getElementById('image-preview');
-    const previewImg = document.getElementById('preview-img');
 
-    navigator.mediaDevices.getUserMedia({ video: true })
-        .then(function (stream) {
-            video.srcObject = stream;
-            video.play();
-        });
 
-    video.addEventListener('loadedmetadata', function () {
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
+   // Function to handle capturing an image
+   function captureImage() {
+       const video = document.createElement("video");
+       const canvas = document.createElement("canvas");
+       const context = canvas.getContext('2d');
+       const imagePreview = document.getElementById('image-preview');
+       const previewImg = document.getElementById('preview-img');
 
-        const intervalId = setInterval(function () {
-            context.drawImage(video, 0, 0, canvas.width, canvas.height);
-            previewImg.src = canvas.toDataURL('image/png');
-        }, 100);
+       navigator.mediaDevices.getUserMedia({ video: true })
+           .then(function (stream) {
+               video.srcObject = stream;
+               video.play();
+           });
 
-        // Clear interval and release resources when done
-        setTimeout(function () {
-            clearInterval(intervalId);
-            video.srcObject.getTracks().forEach(track => track.stop());
-            imagePreview.removeChild(video);
-            imagePreview.appendChild(canvas);
+       video.addEventListener('loadedmetadata', function () {
+           canvas.width = video.videoWidth;
+           canvas.height = video.videoHeight;
 
-            canvas.toBlob(function (blob) {
-                const reader = new FileReader();
-                reader.onloadend = function () {
-                    sendToBackend(reader.result);
-                };
-                reader.readAsDataURL(blob);
-            }, 'image/png');
-        }, 3000); // Example: Capture image for 3 seconds
-    });
-}
+           const intervalId = setInterval(function () {
+               context.drawImage(video, 0, 0, canvas.width, canvas.height);
+               previewImg.src = canvas.toDataURL('image/png');
+           }, 100);
 
-// Function to send data to the backend
-function sendToBackend(imageData) {
-    // Sanitize and validate the imageData if needed
+           // Clear interval and release resources when done
+           setTimeout(function () {
+               clearInterval(intervalId);
+               video.srcObject.getTracks().forEach(track => track.stop());
+               video.remove(); // Use `remove()` to remove the video element
+               imagePreview.appendChild(canvas); // Append canvas directly
 
-    document.getElementById('loading').style.display = 'block';
+               canvas.toBlob(function (blob) {
+                   const reader = new FileReader();
+                   reader.onloadend = function () {
+                       imageData = reader.result;
+                   };
+                   reader.readAsDataURL(blob);
+               }, 'image/png');
+           }, 3000);
+       });
+   }
 
-    fetch('/process_image', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: 'image=' + encodeURIComponent(imageData),
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.blob();
-        })
-        .then(blob => {
-            const url = window.URL.createObjectURL(blob);
-            const a = document.createElement('a');
-            a.href = url;
-            a.download = 'extracted_images.zip';
-            document.body.appendChild(a);
-            a.click();
-            window.URL.revokeObjectURL(url);
-            document.getElementById('loading').style.display = 'none';
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            document.getElementById('loading').style.display = 'none';
-            // Show user-friendly error message if needed
-        });
-}
+   // Function to send data to the backend
+   async function sendToBackend(imageData) {
+       // Sanitize and validate the imageData if needed
 
-// Event listeners
-document.getElementById('upload-btn').addEventListener('click', () => {
-    document.getElementById('upload-input').click();
+       document.getElementById('loading').style.display = 'block';
+       const loadingSpinner = document.getElementById('loading');
+       loadingSpinner.style.display = 'inline-block'; // Display spinner
+
+       try {
+           const response = await fetch('/process_image', {
+               method: 'POST',
+               headers: {
+                   'Content-Type': 'application/x-www-form-urlencoded',
+               },
+               body: 'image=' + encodeURIComponent(imageData),
+           });
+
+           if (!response.ok) {
+               throw new Error('Network response was not ok');
+           }
+
+           const blob = await response.blob();
+           const url = window.URL.createObjectURL(blob);
+           const a = document.createElement('a');
+           a.href = url;
+           a.download = 'extracted_images.zip';
+           document.body.appendChild(a);
+           a.click();
+           window.URL.revokeObjectURL(url);
+       } catch (error) {
+           console.error('Error:', error);
+           // Show user-friendly error message if needed
+       } finally {
+           document.getElementById('loading').style.display = 'none';
+       }
+   }
+
+   // Event listeners
+   document.getElementById('upload-input').addEventListener('change', handleImageUpload);
+   document.getElementById('capture-btn').addEventListener('click', captureImage);
+   document.getElementById('submit-btn').addEventListener('click', () => {
+       sendToBackend(imageData);
+       console.log("send data")
+     }
+   )
+   document.getElementById('remove-btn').addEventListener('click', async() => {
+    document.getElementById('preview-img').src = "https://cdn.vectorstock.com/i/preview-1x/90/36/add-user-icon-design-blank-avatar-placeholder-vector-34269036.webp";
+     
 });
-
-document.getElementById('upload-input').addEventListener('change', handleImageUpload);
-document.getElementById('capture-btn').addEventListener('click', captureImage);
-
-document.getElementById('upload-btn').addEventListener('click', () => {
-    document.getElementById('upload-input').click();
-});
-
-document.getElementById('upload-input').addEventListener('change', handleImageUpload);
-document.getElementById('capture-btn').addEventListener('click', captureImage);
-
